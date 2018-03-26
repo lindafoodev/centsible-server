afd0eef823df8c18e5b62711014665cae10f089d
John Castillo <JohnluisCastillo@gmail.com>
2018-03-26 16:52:25 -0400

    end of day

--- users/models.js
+++ users/models.js
@@ -14,15 +14,37 @@ const UserSchema = mongoose.Schema({
     required: true
   },
   firstName: {type: String, default: ''},
-  lastName: {type: String, default: ''}
+  lastName: {type: String, default: ''},
+  // bday: {type:Date, required: true},
+  // email: {type:String, required: true},
+  // game: {
+  //   level: {type: Number, default: 1},
+  //   initialFund: {type: Number, default: 5000},
+  //   currentFund: {type: Number},
+  //   risk:{
+  //     1: {type: String},
+  //     2: {type: String},
+  //     3: {type: String},
+  //     4: {type: String},
+  //     5: {type: String}
+  //   },
+  //   year: {type: Number, default: 1}
+  // }
 });
-
+// I'M SO LOST
 UserSchema.methods.serialize = function() {
   return {
     username: this.username || '',
     firstName: this.firstName || '',
     lastName: this.lastName || '',
     id: this._id
+    // bday: this.bday,
+    // email: this.email,
+    // level: this.game.level,
+    // initialFund: this.game.initialFund,
+    // currentFund: this.game.currentFund,
+    // risk: this.game.risk,
+    // year: this.game.year
   };
 };
 
